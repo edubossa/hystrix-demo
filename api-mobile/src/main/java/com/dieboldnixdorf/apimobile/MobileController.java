138b8ff5f15e4f4559b7b8c38ce8f7a2be853cb9
Eduardo Wallace <edubossa@gmail.com>
2018-07-31 17:01:50 -0300

    Alter config Api Modile

--- api-mobile/src/main/java/com/dieboldnixdorf/apimobile/MobileController.java
+++ api-mobile/src/main/java/com/dieboldnixdorf/apimobile/MobileController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/mobile")
 public class MobileController {
 
     @Autowired
